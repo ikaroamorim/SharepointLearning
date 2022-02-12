01da4de3fa1a95949051e724582bf2c3f0d41305
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2022-02-12 15:28:55 -0300

    Creation of the first console app consuming Sharepoint Info Deletion of old solution file

---
+++ Udemy Mastering SP2013  Csharp/01FarmInfo/FarmInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace FarmInfo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      SPFarm farm = SPFarm.Local;
+      Console.WriteLine($"Farm Id: {farm.Id} \nFarm Name: {farm.Name} \nFarm Status: {farm.Status} \n");
+      Console.ReadKey();
+    }
+  }
+}
